4310f5f00f71a31a34e89d9d19799bf5aa05ca5d
MichellR14 <delfina.recarte@ujcv.edu.hn>
2016-04-15 21:08:29 -0600

    Registrar usuarios
    avance para registrar usuarios.

--- StudyApp/app/src/main/java/com/example/joseamaya/studyapp/MainActivity.java
+++ StudyApp/app/src/main/java/com/example/joseamaya/studyapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.joseamaya.studyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -33,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void detalle_michell(View v){
+        Intent intento=new Intent(this, Registro.class);
+        startActivity(intento);
     }
 
     @Override
--- StudyApp/app/src/main/java/com/example/joseamaya/studyapp/Registro.java
+++ StudyApp/app/src/main/java/com/example/joseamaya/studyapp/Registro.java
@@ -1,13 +1,47 @@
 package com.example.joseamaya.studyapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Registro extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener{
+    EditText nombre;
+    EditText apellido;
+    EditText correo;
+    EditText telefono;
+    EditText contraseña;
+    Button guardar;
+
+    // IP de mi Url
+    String IP = "http://studyaplication.esy.es";
+    // Rutas de los Web Services
+    String INSERT = IP + "/insertar_maestro.php";
+
+    ObtenerWebService hiloconexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +58,152 @@ public class Registro extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Enlace con los elementos
+        nombre=(EditText)findViewById(R.id.editText);
+        apellido=(EditText)findViewById(R.id.editText2);
+        correo=(EditText)findViewById(R.id.editText3);
+        telefono=(EditText)findViewById(R.id.editText4);
+        contraseña=(EditText)findViewById(R.id.editText5);
+        guardar=(Button)findViewById(R.id.button);
+
+        //Listener de los botones
+        nombre.setOnClickListener(this);
+        apellido.setOnClickListener(this);
+        correo.setOnClickListener(this);
+        telefono.setOnClickListener(this);
+        contraseña.setOnClickListener(this);
+        guardar.setOnClickListener(this);
     }
 
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button){
+            hiloconexion = new ObtenerWebService();
+            hiloconexion.execute(INSERT,"3","001",nombre.getText().toString(),apellido.getText().toString(),correo.getText().toString(),
+                    telefono.getText().toString(),contraseña.getText().toString());   // Parámetros que recibe doInBackground
+
+        }
+
+    }
+
+    public class ObtenerWebService extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String cadena = params[0];
+            URL url = null; // Url de donde queremos obtener información
+            String devuelve ="";
+
+
+
+            if(params[1]=="1"){    // Consulta de todos los Maestros
+
+            }
+            else if(params[1]=="2"){    // consulta por id
+
+
+            }
+            else if(params[1]=="3"){    // insert
+
+                try {
+                    HttpURLConnection urlConn;
+
+                    DataOutputStream printout;
+                    DataInputStream input;
+                    url = new URL(cadena);
+                    urlConn = (HttpURLConnection) url.openConnection();
+                    urlConn.setDoInput(true);
+                    urlConn.setDoOutput(true);
+                    urlConn.setUseCaches(false);
+                    urlConn.setRequestProperty("Content-Type", "application/json");
+                    urlConn.setRequestProperty("Accept", "application/json");
+                    urlConn.connect();
+                    //Creo el Objeto JSON
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("cod_maestro", params[2]);
+                    jsonParam.put("nombre", params[3]);
+                    jsonParam.put("apellido", params[4]);
+                    jsonParam.put("email", params[5]);
+                    jsonParam.put("telefono", params[6]);
+                    jsonParam.put("password", params[7]);
+                    // Envio los parámetros post.
+                    OutputStream os = urlConn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(jsonParam.toString());
+                    writer.flush();
+                    writer.close();
+
+                    int respuesta = urlConn.getResponseCode();
+
+
+                    StringBuilder result = new StringBuilder();
+
+                    if (respuesta == HttpURLConnection.HTTP_OK) {
+
+                        String line;
+                        BufferedReader br=new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                        while ((line=br.readLine()) != null) {
+                            result.append(line);
+                            //response+=line;
+                        }
+
+                        //Creamos un objeto JSONObject para poder acceder a los atributos (campos) del objeto.
+                        JSONObject respuestaJSON = new JSONObject(result.toString());   //Creo un JSONObject a partir del StringBuilder pasado a cadena
+                        //Accedemos al vector de resultados
+
+                        String resultJSON = respuestaJSON.getString("estado");   // estado es el nombre del campo en el JSON
+
+                        if (resultJSON == "1") {      // hay un Maestro que mostrar
+                            devuelve = "Maestro insertado correctamente";
+
+                        } else if (resultJSON == "2") {
+                            devuelve = "El maestro no pudo insertarse";
+                        }
+
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return devuelve;
+
+
+            }
+            else if(params[1]=="4"){    // update
+
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            super.onCancelled(s);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //resultado.setText(s);
+            //super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+    }
 }
